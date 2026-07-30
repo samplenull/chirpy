d6039d796f0ed92734ebaab0988e5c404eb14d94
Denis sampleNull <samplenull@gmail.com>
2026-07-30 15:02:58 +0300

    add simple index.html serving

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	// Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
+	// Use a standard http.FileServer as the handler
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	server.ListenAndServe()
 }
