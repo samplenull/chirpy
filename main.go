a773b6058a460554356dec68d3ed0a0d84b6b48d
Denis sampleNull <samplenull@gmail.com>
2026-08-13 13:33:51 +0300

    add logging and assets serving

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Server starting...")
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	// Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
-	// Use a standard http.FileServer as the handler
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	server.ListenAndServe()
+
+	mux.HandleFunc("/healthz", healthzHandler)
+
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", loggingHandler(http.StripPrefix("/app/", fs)))
+
+	fmt.Println("Server is listening on :8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
+}
+
+func loggingHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Serving %s at %s\n", r.URL.Path, r.Method)
+		next.ServeHTTP(w, r)
+	})
 }
